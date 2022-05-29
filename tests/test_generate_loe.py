dacfd8ffb448275abce40130d98364ac9deb0497
Timothy Hull <timothyhull@gmail.com>
2022-05-29 08:53:09 +0000

    Refactored to mock the call to generate_loe.main

--- tests/test_generate_loe.py
+++ tests/test_generate_loe.py
@@ -2,26 +2,40 @@
 """ pytest tests for /app/generate_loe.py """
 
 # Imports - Python Standard Library
+from unittest.mock import MagicMock, patch
 
 # Imports - Third-Party
+# None
 
 # Imports - Local
-from app.generate_loe import main
+from app import generate_loe
 
 # Constants
+# None
 
 
-def test_main() -> None:
+@patch.object(
+    target=generate_loe,
+    attribute='main'
+)
+def test_main(
+    main_mock: MagicMock
+) -> None:
     """ Test the main function.
 
         Args:
-            None.
+            main_mock (unittest.mock.MainMock):
+                Mock return value for the generate_loe.main
+                function.
 
         Returns:
             None.
     """
 
+    # Set the return value for main_mock
+    main_mock.return_value = None
+
     # Confirm the read_source_data function returns the mock return value
-    assert main() is None
+    assert generate_loe.main() is None
 
     return None
